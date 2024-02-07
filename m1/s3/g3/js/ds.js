5b75220206863f3533bd202873a4c97061351987
DottDre <acquarius33@gmail.com>
2024-02-07 16:55:03 +0100

    js7

---
+++ m1/s3/g3/js/ds.js
@@ -0,0 +1,18 @@
+const aggiungi = document.getElementById('aggiungi')
+aggiungi.addEventListener('click', function (e) {
+    e.preventDefault()
+})
+let div = document.createElement('div')
+let input = document.getElementById('input')
+let form = document.getElementById('form')
+
+
+aggiungi.addEventListener('click',function (e){
+
+    
+    e.div
+    div.innerText = input.value
+    form.append(div)
+    //console.log(div)
+}
+)
