b5dba052e54aa444d98230eee044949b4c91b814
DottDre <acquarius33@gmail.com>
2024-02-08 16:19:17 +0100

    js8

--- m1/s3/g4/js/ds.js
+++ m1/s3/g4/js/ds.js
@@ -1,10 +1,26 @@
 let tombola = document.querySelector(".tombola");
 
-    for (let i = 1; i <= 76; i++) {
+    for (let i = 1; i <= 90; i++) {
         let cella = document.createElement("div")
         cella.classList.add("tabella")
         cella.innerText = i
         tombola.appendChild(cella)
 }
-console.log(tombola)
+//console.log(tombola)
 
+let btn = document.querySelector(".bottone")
+function numeroEstrazione() {
+     let a = Math.floor(Math.random() * 89)+1
+     return a
+    
+}
+//console.log(numeroEstrazione())
+btn.addEventListener('click', ()=>{
+    let b = numeroEstrazione()
+    selectcella(b)
+    }
+)
+
+
+
+//console.log(b)
