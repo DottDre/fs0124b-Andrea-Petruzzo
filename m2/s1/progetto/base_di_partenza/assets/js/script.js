9d210a1d09c965503d746392660f7fdd21cb8d63
DottDre <acquarius33@gmail.com>
2024-02-24 15:03:22 +0100

    progetto

--- m2/s1/progetto/base_di_partenza/assets/js/script.js
+++ m2/s1/progetto/base_di_partenza/assets/js/script.js
@@ -1,9 +1,12 @@
 function CambiaColore() { 
-    const header = document.querySelector(".header")
+    const header = document.querySelector('.header')
+    const button = document.querySelector('.button-black')
     if (document.body.scrollTop > 350 || document.documentElement.scrollTop > 350) { 
         header.style.backgroundColor = "white"
+        button.style.backgroundColor = "green"
     } else { 
         header.style.backgroundColor = " #FFC017"
+        button.style.backgroundColor = "black"
     } 
 }
 window.addEventListener('scroll', CambiaColore);
