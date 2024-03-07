b87e8eee3288360696d36121a08a462cc1a0c14f
DottDre <acquarius33@gmail.com>
2024-03-07 15:40:11 +0100

    fetch2

---
+++ m2/s3/g4/js/script.js
@@ -0,0 +1,25 @@
+
+async function chiamata(query){
+
+    const apiKey = '3Er1dMjCTN5c4LXJ7uNuCB9XIOWDbw90tdixMwllcnGYXZoSBs5DxZJh';
+    
+    return await fetch(`https://api.pexels.com/v1/search?query=${query}`, {
+        method: 'GET',
+        headers: {
+            Authorization: apiKey
+        }
+    }).then(rep => rep.json())
+}
+      let btn = document.querySelector(".btn");
+      btn.addEventListener("click", () => {
+        chiamata("donne").then((dati) => {
+          console.log(dati);
+          let immagini = document.querySelectorAll(".card-img-top");
+          dati.photos.forEach((photo, i) => {
+              immagini[i].src = photo.src.medium;
+            });
+        });
+      });
+        
+
+
