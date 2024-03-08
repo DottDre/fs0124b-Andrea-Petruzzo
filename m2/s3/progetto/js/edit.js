1e91714618c10855f23ea235f56e3d7e8ccfe1e7
DottDre <acquarius33@gmail.com>
2024-03-08 16:06:36 +0100

    index+create+edit

--- m2/s3/progetto/js/edit.js
+++ m2/s3/progetto/js/edit.js
@@ -1,11 +1,56 @@
 let url = new URLSearchParams(location.search)
-let id = url.get('_id')
+let id = url.get('id')
 console.log(id)
 
 fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`,{
-    method: 'PUT',
     headers: {
         'Content-type':'application/json',
         Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZGEzYjJkN2IxMTAwMTkwZTZkZWYiLCJpYXQiOjE3MDk4OTAxMDcsImV4cCI6MTcxMTA5OTcwN30.zsDD5gyVPTazmEJrT1GPgji7WoRLj4nP93cUIwb1q3w"
     },
 })
+.then(res => res.json())
+.then(phone =>{
+
+    let name = document.querySelector('#nome')
+    name.value = phone.name
+    let brand = document.querySelector('#brand')
+    brand.value = phone.brand
+    let price = document.querySelector('#prezzo')
+    price.value = phone.price
+    let imageUrl = document.querySelector('#image')
+    imageUrl = phone.imageUrl
+    let description = document.querySelector('#description')
+    description.value = phone.description
+}) 
+
+let salvaBtn = document.querySelector('#salva')
+salvaBtn.addEventListener('click', (e) => {
+    e.preventDefault()
+    let name = document.querySelector('#nome').value
+    let brand = document.querySelector('#brand').value
+    let price = document.querySelector('#prezzo').value
+    let imageUrl = document.querySelector('#image').value
+    let description = document.querySelector('#description').value
+
+    let phone = {
+        name,
+        description,
+        brand,
+        imageUrl,
+        price
+        
+    }
+
+    fetch(`https://striveschool-api.herokuapp.com/api/product/${id}`,{
+                method:'PUT',
+                headers:{
+                    'Content-type':'application/json',
+                    "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZTcyMTJkN2IxMTAwMTkwZTZmMTQiLCJpYXQiOjE3MDk4OTM0MDksImV4cCI6MTcxMTEwMzAwOX0.HZkeQ2N6C9nMLUq1ZoUdGC9k-Za4JnzyHVzNQGXD0Pw"
+                },
+                body:JSON.stringify(phone)
+            })
+            .then(res => res.json())
+            .then(res => {
+                location.href = 'index.html'
+            })
+}) 
--- m2/s3/progetto/js/index.js
+++ m2/s3/progetto/js/index.js
@@ -1,7 +1,7 @@
 fetch("https://striveschool-api.herokuapp.com/api/product/",{
     headers: {
         'Content-type':'application/json',
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZGEzYjJkN2IxMTAwMTkwZTZkZWYiLCJpYXQiOjE3MDk4OTAxMDcsImV4cCI6MTcxMTA5OTcwN30.zsDD5gyVPTazmEJrT1GPgji7WoRLj4nP93cUIwb1q3w"
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NWVhZGEzYjJkN2IxMTAwMTkwZTZkZWYiLCJpYXQiOjE3MDk4OTAxMDcsImV4cCI6MTcxMTA5OTcwN30.zsDD5gyVPTazmEJrT1GPgji7WoRLj4nP93cUIwb1q3w "
     },
 
 })
