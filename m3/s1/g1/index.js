d55731cdebd18241072bc9e5ec48dd0506473250
DottDre <acquarius33@gmail.com>
2024-03-18 16:19:38 +0100

    extra fatto

--- m3/s1/g1/index.js
+++ m3/s1/g1/index.js
@@ -11,6 +11,9 @@ class User {
         //console.log(this.ricarica(10)) // capire perchè non compare in console: ok non andava fatto qui
     }
     chiamata(n) {
+        if (this.credito - (n * 0.20) < 0) {
+            return console.log('credito insufficente');
+        }
         this.credito -= (n * 0.20);
         this.numeroChiamate += 1;
         //console.log(this.chiamata(3)) // capire anche questo idem a sopra
@@ -26,11 +29,17 @@ class User {
     }
 }
 const user = new User('andrea', 'petruzzo');
-user.ricarica(10);
+user.ricarica(8);
 user.chiamata(3);
 user.chiamata(3);
 user.chiamata(1);
+user.chiamata(8);
 console.log(user.chiamata404);
 console.log(user.numeroChiamate);
 user.azzeraChiamate();
+user.chiamata(3);
+user.chiamata(3);
+user.chiamata(20);
+console.log(user.chiamata404);
+console.log(user.numeroChiamate);
 //# sourceMappingURL=index.js.map
