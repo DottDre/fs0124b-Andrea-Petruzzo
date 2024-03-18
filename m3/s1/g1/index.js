2704f8b3745f5006bd12a51846b146a77c5c4e38
DottDre <acquarius33@gmail.com>
2024-03-18 15:54:57 +0100

    finito senza extra

--- m3/s1/g1/index.js
+++ m3/s1/g1/index.js
@@ -3,18 +3,34 @@ class User {
     constructor(nome, cognome) {
         this.credito = 0;
         this.numeroChiamate = 0;
-        this.chiamata404 = this.credito;
-        this.getNumeroChiamata = this.numeroChiamate;
         this.nome = nome;
         this.cognome = cognome;
     }
     ricarica(a) {
         this.credito += a;
-        console.log(this.ricarica(10)); // capire perchè non compare in console
+        //console.log(this.ricarica(10)) // capire perchè non compare in console: ok non andava fatto qui
     }
     chiamata(n) {
-        this.credito - (n * 0.20);
-        console.log(this.chiamata(3)); // capire anche questo
+        this.credito -= (n * 0.20);
+        this.numeroChiamate += 1;
+        //console.log(this.chiamata(3)) // capire anche questo idem a sopra
+    }
+    get chiamata404() {
+        return this.credito;
+    }
+    get getNumeroChiamata() {
+        return this.numeroChiamate;
+    }
+    azzeraChiamate() {
+        this.numeroChiamate = 0;
     }
 }
+const user = new User('andrea', 'petruzzo');
+user.ricarica(10);
+user.chiamata(3);
+user.chiamata(3);
+user.chiamata(1);
+console.log(user.chiamata404);
+console.log(user.numeroChiamate);
+user.azzeraChiamate();
 //# sourceMappingURL=index.js.map
--- m3/s1/g1/index.ts
+++ m3/s1/g1/index.ts
@@ -4,8 +4,9 @@ interface Smartphone {
 
     ricarica(a:number): void
     chiamata(n:number): void
-    chiamata404:number 
-    getNumeroChiamata: number
+    get chiamata404():number 
+    get getNumeroChiamata():number
+    azzeraChiamate(): void
 }
 
 class User implements Smartphone {
@@ -20,15 +21,32 @@ class User implements Smartphone {
 
     ricarica(a: number): void{
         this.credito += a
-        console.log(this.ricarica(10)) // capire perchè non compare in console
+        //console.log(this.ricarica(10)) // capire perchè non compare in console: ok non andava fatto qui
     }
     chiamata(n: number): void {
-        this.credito - (n * 0.20)
-        console.log(this.chiamata(3)) // capire anche questo
+        this.credito -= (n * 0.20)
+        this.numeroChiamate += 1
+        //console.log(this.chiamata(3)) // capire anche questo idem a sopra
     }
 
-    chiamata404: number = this.credito
-    getNumeroChiamata: number = this.numeroChiamate
+    get chiamata404():number {
+        return this.credito
+    }
+        
+    get getNumeroChiamata():number {
+        return this.numeroChiamate
+    } 
 
-    
+    azzeraChiamate(): void {
+        this.numeroChiamate = 0
+    }
 }
+    const user = new User ('andrea', 'petruzzo')
+        user.ricarica(10)
+        user.chiamata(3)
+        user.chiamata(3)
+        user.chiamata(1)
+        console.log(user.chiamata404)
+        console.log(user.numeroChiamate)
+        user.azzeraChiamate()
+        
