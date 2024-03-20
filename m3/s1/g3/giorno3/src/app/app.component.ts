a3a83d9c788ba48c748568f247e55196e87c9bc5
DottDre <acquarius33@gmail.com>
2024-03-20 16:34:14 +0100

    prove

--- m3/s1/g3/giorno3/src/app/app.component.ts
+++ m3/s1/g3/giorno3/src/app/app.component.ts
@@ -8,10 +8,22 @@ import { Nb1 } from './models/nb1';
 })
 export class AppComponent {
   dpArr:Nb1[] = []
+  randomPost:Nb1[]=[]
+  ran: number = 0
+  ngOnInit(){
+    fetch('../assets/db.json')
+    .then(dati => dati.json())
+    .then( (dati) => (this.dpArr = dati.posts) )
+  }
+  Random(){
+    let random = Math.floor(Math.random() * this.dpArr.length)
+    this.ran = random
+    return random
+  }
+  get4(){
+    for (let i = 0; i < 4; i++) {
+      this.randomPost.push(this.dpArr[this.Random()])
 
-    ngOnInit(){
-      fetch('../assets/db.json')
-      .then(dati => dati.json())
-      .then( (dati) => (this.dpArr = dati.posts) )
+    }
     }
 }
