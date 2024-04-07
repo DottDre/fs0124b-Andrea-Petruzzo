0ff49f3c739b2c5623ba64a35f7f943c00ed9a32
DottDre <acquarius33@gmail.com>
2024-04-07 19:04:21 +0200

    committino

--- m3/s3/progetto/progetto/src/app/auth/register/register.component.ts
+++ m3/s3/progetto/progetto/src/app/auth/register/register.component.ts
@@ -20,7 +20,7 @@ export class RegisterComponent {
     this.authSvc.register(this.registerData)
     .subscribe(data => {
 
-      this.router.navigate(['/auth/login'])
+      this.router.navigate(['/'])
 
     })
   }
