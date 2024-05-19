779a114afd5ceabd080a0eadf7c3ce794f60de85
DottDre <acquarius33@gmail.com>
2024-05-19 17:20:51 +0200

    committino

--- backend/m2/s1/GestionePrenotazioni/src/main/java/it/epicode/runners/SampleRunner.java
+++ backend/m2/s1/GestionePrenotazioni/src/main/java/it/epicode/runners/SampleRunner.java
@@ -9,9 +9,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
 
 @Component
 @Slf4j
@@ -26,31 +27,101 @@ public class SampleRunner implements CommandLineRunner {
     ReservationRepository reservations;
     @Override
     public void run(String... args) throws Exception {
-        var user = users.save(User.builder()
-                .withUsername("DottDre")
-                .withFullName("Andrea Petruzzo")
-                .withEmail("acqaurius33@gmail.com")
-                .build());
-        var building = buildings.save(Building.builder()
-                .withCity("Roma")
-                .withName("atlante")
-                .withAddress("atlente@gmail.com")
-                .withUser(user)
-                .build());
-        var location = locations.save(Location.builder()
-                .withType(Type.PRIVATE)
-                .withFree(true)
-                .withDescription("molto bello")
-                .withMaxOccupants(20L)
-                .withUniqueCode("23343435")
-                .withBuilding(building)
-                .build());
-        var reservation = reservations.save(Reservation.builder()
+        createUsers();
+        createBuildings();
+        createLocation();
+
+        User user = users.findById(1L).orElseThrow(() -> new Exception("Utente non trovato"));
+        Location location = locations.findById(1L).orElseThrow(() -> new Exception("Postazione non trovata"));
+        LocalDate date = LocalDate.now().plusDays(1);
+
+        try {
+            makeReservation(user, location, date);
+            System.out.println("Prenotazione effettuata con successo per l'utente " + user.getUsername());
+        } catch (Exception e) {
+            System.err.println("Impossibile effettuare la prenotazione: " + e.getMessage());
+        }
+
+        createReservations();
+    }
+    private void createUsers() {
+        for (int i = 1; i <= 10; i++) {
+            users.save(User.builder()
+                    .withUsername("username" + i)
+                    .withFullName("Nome Cognome" + i)
+                    .withEmail("email" + i )
+                    .build());
+        }
+    }
+
+    private void createBuildings() {
+        for (int i = 1; i <= 10; i++) {
+             buildings.save(Building.builder()
+                    .withName("Nome Edificio" + i)
+                    .withAddress("Indirizzo Edificio" + i)
+                    .withCity("Città" + i)
+                    .build());
+        }
+    }
+
+    private void createLocation() {
+        List<Building> building = buildings.findAll();
+        Random random = new Random();
+        for (int i = 1; i <= 10; i++) {
+            Building building1 = building.get(random.nextInt(building.size()));
+            locations.save(Location.builder()
+                    .withUniqueCode("codice" + i)
+                    .withDescription("Descrizione" + i)
+                    .withType(Type.PRIVATE)
+                    .withMaxOccupants(5L)
+                    .withBuilding(building1)
+                    .build());
+        }
+    }
+
+    public  void makeReservation(User user, Location location, LocalDate date) throws Exception {
+        if (!isStationAvailable(location, date)) {
+            throw new Exception("La postazione non è disponibile per quella data.");
+        }
+
+        if (hasReservationForDate(user, date)) {
+            throw new Exception("Hai già una prenotazione per questa data.");
+        }
+
+        reservations.save(Reservation.builder()
                 .withUser(user)
                 .withLocation(location)
-                .withBookingDate(LocalDate.now())
+                .withBookingDate(date)
                 .build());
     }
 
+    private boolean isStationAvailable(Location station, LocalDate date) {
+        int count = locations.countReservationForLocationAndDate(station, date);
+        return count == 0;
+    }
 
+    private boolean hasReservationForDate(User user, LocalDate date) {
+        int count = users.countReservationsForUserAndDate(user, date);
+        return count > 0;
+    }
+
+    private void createReservations() {
+        List<User> user1 = users.findAll();
+        List<Location> location = locations.findAll();
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            User user = user1.get(random.nextInt(user1.size()));
+            Location location1 = location.get(random.nextInt(location.size()));
+            LocalDate date = LocalDate.now().plusDays(random.nextInt(30));
+            try {
+                makeReservation(user, location1, date);
+                System.out.println("Prenotazione effettuata con successo per l'utente " + user.getUsername() + " per la postazione " + location1.getUniqueCode());
+            } catch (Exception e) {
+                System.err.println("Impossibile effettuare la prenotazione per l'utente " + user.getUsername() + ": " + e.getMessage());
+            }
+        }
+    }
 }
+
+
+
