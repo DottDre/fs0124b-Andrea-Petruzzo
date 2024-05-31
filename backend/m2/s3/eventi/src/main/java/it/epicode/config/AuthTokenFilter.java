c7e99196c3343ed6e08bafac022db46824ca278f
DottDre <acquarius33@gmail.com>
2024-05-31 16:11:45 +0200

    committino8

--- backend/m2/s3/eventi/src/main/java/it/epicode/config/AuthTokenFilter.java
+++ backend/m2/s3/eventi/src/main/java/it/epicode/config/AuthTokenFilter.java
@@ -15,34 +15,41 @@ import org.springframework.web.filter.OncePerRequestFilter;
 import java.io.IOException;
 @Slf4j
 public class AuthTokenFilter extends OncePerRequestFilter {
+
     @Autowired
     JwtUtils jwt;
 
     @Autowired
     ApplicationUserDetailsService userDetailsService;
+
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-            try {
-                log.info("Processing AuthTokenFilter");
+    protected void doFilterInternal(HttpServletRequest request, //
+                                    HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
+            log.info("Processing AuthTokenFilter");
+            // legge lo header
+            // Authorization: Bearer TOKEN
+            var header = request.getHeader("Authorization");
+            if (header != null && header.startsWith("Bearer")) {
+                var token = header.substring(7);
+                log.info("Token: {}", token);
+                var username = jwt.getUsernameFromToken(token);
+                log.info("Username: {}", username);
+                var details = userDetailsService.loadUserByUsername(username);
+                log.info("Details: {}", details);
 
-                var header = request.getHeader("Authorization");
-                if (header != null && header.startsWith("Bearer")) {
-                    var token = header.substring(7);
-                    log.info("Token: {}", token);
-                    var username = jwt.getUsernameFromToken(token);
-                    log.info("Username: {}", username);
-                    var details = userDetailsService.loadUserByUsername(username);
-                    log.info("Details: {}", details);
-                    var auth = new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
-                    auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                    SecurityContextHolder.getContext().setAuthentication(auth);
-                }
-
-            } catch (Exception e) {
-                log.error("Exception in auth filter", e);
+                // a questo punto l'utente è stato completamente individuato
+                // lo devo solo SCRIVERE NEL CONTESTO DI SICUREZZA
+                var auth = new UsernamePasswordAuthenticationToken(details, null, details.getAuthorities());
+                auth.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // INTEGRO IL MIO SISTEMA DI AUTENTICAZIONE NEL CONTESTO SPRING
+                SecurityContextHolder.getContext().setAuthentication(auth);
             }
-            filterChain.doFilter(request, response);
+
+        } catch (Exception e) {
+            log.error("Exception in auth filter", e);
         }
+        filterChain.doFilter(request, response);
     }
-}
+    }
+
